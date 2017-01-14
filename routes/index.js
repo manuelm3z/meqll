49fb8d764c4232a038ff4f233c942a7274705ffa
Manuel Zambrano <mmzmm3z@gmail.com>
2017-01-14 12:29:48 -0300

    Agregando variables de entorno para heroku

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,76 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const twitter = require('twitter');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+	const client = new twitter({
+		consumer_key: process.env.TWITTER_CONSUMER_KEY,
+		consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+		access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+		access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
+	});
+
+	client.get('search/tweets', {q: 'marico el que lo lea', count: 100}, (error, tweets, response) => {
+		if (error) {
+			throw error;
+		}
+
+		let list = [];
+
+		tweets.statuses.map(item => {
+			let data = {};
+
+			data.text = item.text;
+
+			if (item.entities.urls.length > 0) {
+				data.link = item.entities.urls[0].expanded_url;
+			}
+
+			list.push(data);
+		});
+
+		let number = getRandomInt(0, 99);
+
+		res.send(list[number]);
+	});
 });
+
+router.get('/api', (req, res, next) => {
+	const client = new twitter({
+		consumer_key: process.env.TWITTER_CONSUMER_KEY,
+		consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+		access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+		access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
+	});
+
+	client.get('search/tweets', {q: 'marico el que lo lea', count: 100}, (error, tweets, response) => {
+		if (error) {
+			throw error;
+		}
+
+		let list = [];
+
+		tweets.statuses.map(item => {
+			let data = {};
+
+			data.text = item.text;
+
+			if (item.entities.urls.length > 0) {
+				data.link = item.entities.urls[0].expanded_url;
+			}
+
+			list.push(data);
+		});
+
+		let number = getRandomInt(0, 99);
+
+		res.send(list[number]);
+	});
+});
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
 
 module.exports = router;
